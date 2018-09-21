980c9e9e9973abfcf93e609d3baf9357e7994a04
Fchen <mario@fridrichovsky.at>
2018-09-21 23:41:05 +0200

    initiated promise

--- functions/portscan.js
+++ functions/portscan.js
@@ -2,6 +2,6 @@ const path = require("path");
 const spawn = require("child_process");
 const parseXML = require("xml2js").parseString;
 
-module.exports = (ip) => {
-
-}
+module.exports = (ip) => new Promise(function (resolve, reject) {
+    console.log(ip);
+});
